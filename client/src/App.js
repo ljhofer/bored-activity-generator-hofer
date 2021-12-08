39d67b5b261dab71428c0bf4d6505f7760794516
Laura Hofer <ljhofer@gmail.com>
2021-12-08 14:49:40 -0600

    Added components, pages, and config folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/product/:id" component={Product} />
+          <Route path='*'>
+          </Route> */}
+      </div>
+    </Router>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// Use only if the main html isn't loading in production
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
 });
