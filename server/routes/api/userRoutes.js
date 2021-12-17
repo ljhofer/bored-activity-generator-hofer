31ea9d6ee6215c9d1a157c5231f6ee5df1fee9f1
Laura Hofer <ljhofer@gmail.com>
2021-12-17 09:34:22 -0600

    Added routes to userRoutes.js

---
+++ server/routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const { createUser, getUserById, updateUser, login } = require("../../controllers/UserController");
+
+router.route("/").post(createUser);
+router.route("/:id").get(getUserById);
+router.route("/:id").put(updateUser);
+router.route("/login").post(login);
+
+module.exports = router; 
+
