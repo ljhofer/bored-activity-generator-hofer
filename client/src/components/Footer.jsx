27980e1976a8fd40a0f53b8a15483a66bff66b1c
Laura Hofer <ljhofer@gmail.com>
2021-12-20 11:34:20 -0600

    Added FontAwesome, React Icons, and React Bootstrap

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+// Styling for the HTML elements generated with this component
+const styles = {
+    footerIcon: {
+      margin: 25,
+      fontSize: '2em',
+      color: 'black'
+    },
+    footer: {
+      backgroundColor: '#8d968e',
+    }
+  };
 
 function Footer () {
 
@@ -6,10 +19,26 @@ function Footer () {
 
 
     return (
-        <div>
-            <h1>I am a footer!</h1>
-
-        </div>
+        <footer style={styles.footer} className="footer">
+            <p>Created by Laura Hofer</p>
+            <div className="container text-center">
+            <a 
+                href="https://github.com/ljhofer" 
+                target="_blank" rel="noreferrer">
+                    <FaGithub style={styles.footerIcon}/>
+            </a>
+            <a 
+                href="https://www.linkedin.com/in/laura-hofer/" 
+                target="_blank" rel="noreferrer">
+                    <FaLinkedin style={styles.footerIcon} />
+            </a>
+            <a 
+                href="mailto:ljhofer@gmail.com" 
+                target="_blank" rel="noreferrer">
+                    <FaEnvelope style={styles.footerIcon} />
+            </a>
+            </div>
+      </footer>
     )
 }
 
