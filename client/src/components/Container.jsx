9e44d4409bef772b8ea700cf8e2a8776fffb2bac
Laura Hofer <ljhofer@gmail.com>
2021-12-29 15:26:25 -0600

    Added React Bootstrap and a card template to SmallCard.jsx. Still working on card styling

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import SmallCard from "./SmallCard";
 
 
 
@@ -16,6 +17,7 @@ function Container () {
     return (
         <div>
             <h1>I am a container!</h1>
+            <SmallCard/>
             <Router>
                 <Routes>
                     <Route exact path="/" component={Home} />
--- client/src/components/SmallCard.jsx
+++ client/src/components/SmallCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Card  from "react-bootstrap/Card";
+import { ListGroup } from "react-bootstrap";
 
 function SmallCard () {
 
@@ -6,9 +8,14 @@ function SmallCard () {
 
     return (
         <div>
-
-
-
+            <Card style={{ width: '18rem' }}>
+                <Card.Header>Featured</Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </Card>
         </div>
     )
 }
