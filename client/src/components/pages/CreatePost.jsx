40d21b73942588fb9b51b6fa586276f80b4d74f0
Laura Hofer <ljhofer@gmail.com>
2022-01-02 15:43:04 -0600

    Added classbased syntax to CreatePost.jsx

--- client/src/components/pages/CreatePost.jsx
+++ client/src/components/pages/CreatePost.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Component } from "react";
 
-function CreatePost () {
+class CreatePost extends Component {
 
+    render() {
+        return (
+            <div>
 
+                <h1>I am the CreatePost componenet!</h1>
 
-    return (
-        <div>
-
-            <h1>I am the CreatePost componenet!</h1>
-
-        </div>
-    )
+            </div>
+        )
+    }       
 }
 
 export default CreatePost;
